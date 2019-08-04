6f729f7969fc96126575c3f057f9f3dd56f2d0b2
Ali Matoug <ali.matoug@softeam.fr>
2019-08-04 17:05:12 +0200

    fix test configuration class

--- src/test/java/com/home/dz/config/ServiceConfigTest.java
+++ src/test/java/com/home/dz/config/ServiceConfigTest.java
@@ -6,7 +6,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-@RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration
 @Import(ServiceConfig.class)
 @ActiveProfiles("test")
